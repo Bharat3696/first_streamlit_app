d63fc2def33739f097c82fe6b4ee7f99093348b5
Bharat3696 <110394744+Bharat3696@users.noreply.github.com>
2022-08-02 00:28:26 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -4,6 +4,12 @@ streamlit.title('This is my sample code')
 
 
 streamlit.header('Footaball')
-streamlit.text(' PSG Messi 30')
+streamlit.text(' 🐔 PSG Messi 30')
 streamlit.text(' MANU CR7 07')              
 streamlit.text(' PSG Neymar 10')               
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')

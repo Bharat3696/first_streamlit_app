6995a4211bbaeb05bd3e95dcd07f5457a5d38774
Bharat3696 <110394744+Bharat3696@users.noreply.github.com>
2022-08-01 19:24:47 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('This is my sample code')
+
+
+streamlit.header('Footaball')
+streamlit.text(' PSG Messi 30')
+streamlit.text(' MANU CR7 07')              
+streamlit.text(' PSG Neymar 10')               
